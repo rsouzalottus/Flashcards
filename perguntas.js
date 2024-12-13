a0fd17ba9ea30f13d89b1967a1f85bfb421e1b5b
HelloWorld <165911213+rsouzalottus@users.noreply.github.com>
2024-12-13 14:12:27 -0300

    versão final

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'Python é uma linguagem de programação.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital da Alemanha?',
+    'A capital da Alemanha é Berlin.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa.'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é a raiz quadrada de 144?',
+    'A raiz quadrada de 144 é 12.'
+)
